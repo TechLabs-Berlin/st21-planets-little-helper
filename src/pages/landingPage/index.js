e07f85396d15da696c3832c032e85c13e56150e1
giuliaeremite <prsgiulia@gmail.com>
2021-05-23 13:22:39 +0200

    added link to form from landing page

--- src/pages/landingPage/index.js
+++ src/pages/landingPage/index.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import {Link} from "react-router-dom"
 
 function LandingPage() {
-    return (
-        <div>
-            <h1>Welcome to Planet's Little Helper</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Welcome to Planet's Little Helper</h1>
+      <p>
+        Challenge yourself to change your habits and become more environmentally
+        conscious.
+      </p>
+      <button><Link to="/form">Get started!</Link></button>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
