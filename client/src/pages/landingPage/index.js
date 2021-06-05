22f18ecec852f38f4519708bd042fee27afe812a
InaDavidova <ina.davidovaa@gmail.com>
2021-06-05 06:43:20 +0300

    creating the landing page and 1 million <div>s  :D

--- client/src/pages/landingPage/index.js
+++ client/src/pages/landingPage/index.js
@@ -1,39 +1,69 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './landingPage.css'
 
 function LandingPage() {
   return (
-    <div>
-      <h1>Join us on our mission to save the planet</h1>
-      <p>
-        Register and receive personalized challenges to save
-        the world. Choose your own pace and own categories where
-        you want to improve your lifestyle.
-      </p>
-      <img src={process.env.PUBLIC_URL + "/earth.png"} width='400' alt='planet earth' />
-      <div>
-        <button>
-          <Link to="/form">Get started</Link>
-        </button>
-        <button>
-          <Link to="/challenges">Go to challenges</Link>
-        </button>
+    <div className='content-box'>
+      <div className='container'>
+        <div className='box-text'>
+          <h1>Join us on our mission to save the planet</h1>
+          <p>
+            Register and receive personalized challenges to save
+            the world. Choose your own pace and own categories where
+            you want to improve your lifestyle.
+          </p>
+          <div>
+            <button className='button-register'>
+              <Link to="/form">Register</Link>
+            </button>
+          </div>
+        </div>
+        <div className='box-img'>
+          <img src={process.env.PUBLIC_URL + "/earth.png"} width='400' alt='planet earth' id='planet-img' />
+        </div>
       </div>
       <div>
         <h2>How does it work?</h2>
-        <p>Learn about the current environmental issues and how you can help.</p>
-        <p>Choose from different categories where you want to make an impact.</p>
-        <p>Receive personalized challenges and monitor the progress.</p>
-        <p>Discover the immediate impact of your actions on the environment.</p>
+        <div className='container2'>
+          <div className='how-text'>
+            <p>Learn about the current environmental issues and how you can help.</p>
+          </div>
+          <div className='how-text'>
+            <p>Choose from different categories where you want to make an impact.</p>
+          </div>
+          <div className='how-text'>
+            <p>Receive personalized challenges and monitor the progress.</p>
+          </div>
+          <div className='how-text'>
+            <p>Discover the immediate impact of your actions on the environment.</p>
+          </div>
+        </div>
       </div>
       <div>
         <h3>Categories</h3>
-        <button><Link to="">Food</Link></button>
-        <button><Link to="">Shopping</Link></button>
-        <button><Link to="">Home & Work</Link></button>
-        <button><Link to="">Energy</Link></button>
-        <button><Link to="">Advocacy</Link></button>
-        <button><Link to="">Transport & Travel</Link></button>
+        <div className='container3'>
+          <div>
+            <button className='btns'><Link to="">Food</Link></button>
+          </div>
+          <div>
+            <button className='btns'><Link to="">Shopping</Link></button>
+          </div>
+          <div>
+            <button className='btns'><Link to="">Home & Work</Link></button>
+          </div>
+        </div>
+        <div className='container3'>
+          <div>
+            <button className='btns'><Link to="">Energy</Link></button>
+          </div>
+          <div>
+            <button className='btns'><Link to="">Advocacy</Link></button>
+          </div>
+          <div>
+            <button className='btns'><Link to="">Transport & Travel</Link></button>
+          </div>
+        </div>
       </div>
     </div>
   );
