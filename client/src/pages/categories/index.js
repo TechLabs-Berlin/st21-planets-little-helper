6a4b5769a24a12baad005dd165251bb9c5cc0148
InaDavidova <ina.davidovaa@gmail.com>
2021-06-06 06:05:12 +0300

    Creating Challenges Page with the 4 Categories

--- client/src/pages/categories/index.js
+++ client/src/pages/categories/index.js
@@ -1,27 +1,40 @@
 import React from 'react'
-import {Link} from "react-router-dom"
+// import { Link } from "react-router-dom"
+import './categories.css'
+
 
 const categories = [
-    {category:"Diet", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", path:"/challenges/food"},
-    {category:"Transportation and Travel", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", path:"/challenges/transportation-and-travel"},
-    {category:"Energy Consumption", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", path:"/challenges/energy-consumption"},
-    {category:"Recycling", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", path:"/challenges/recycling"},
+    { category: "Diet", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", path: "/challenges/food", img: "/food.jpg" },
+    { category: "Travel", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", path: "/challenges/transportation-and-travel", img: "/travel.jpg" },
+    { category: "Energy", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", path: "/challenges/energy-consumption", img: "/energy.jpg" },
+    { category: "Recycling", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", path: "/challenges/recycling", img: "/recycle.jpg" },
 ]
 
 function AllCategories() {
     return (
         <div>
             <h1>All categories:</h1>
-            <ul>
+            <div className='container-div'>
+
                 {categories.map(cat => (
-                    <li key={cat.category}>
-                    <Link to={cat.path}>
+
+                    <div className='category-box'>
+
                         <h4>{cat.category}</h4>
+                        <div className='imgDiv'>
+                            <a href={cat.path}>
+                                <img src={process.env.PUBLIC_URL + "/logo.png"} alt={cat.category} />
+                            </a>
+                        </div>
+
                         <p>{cat.description}</p>
-                    </Link>
-                </li>
+
+
+                    </div>
                 ))}
-            </ul>
+
+
+            </div>
         </div>
     )
 }
