14d5b0eda84ba3bac520058e9929a99a94f25386
giuliaeremite <prsgiulia@gmail.com>
2021-06-07 11:10:35 +0200

    added necessary depedencies, created store and 2 reducers: error and current user, added current user to navbar

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -1,45 +1,67 @@
-import React from 'react'
-import { NavLink } from "react-router-dom"
-import './header.css'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { connect } from "react-redux";
+import "./header.css";
 
 const links = [
-    { page: "Challenges", path: "/challenges" },
-    { page: "Sign in", path: "/form" },
-    { page: "Sign up", path: "/form" },
-    { page: "User Profile", path: "/user/1" }
-]
+  { page: "Challenges", path: "/challenges" },
+  { page: "Sign in", path: "/form" },
+  { page: "Sign up", path: "/form" },
+  { page: "User Profile", path: "/user/1" },
+];
 
 class Header extends React.Component {
-    state = { clicked: false }
+  state = { clicked: false };
 
-    handleClick = () => {
-        this.setState({ clicked: !this.state.clicked })
-    }
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
 
-    render() {
-        return (
-            <header>
-                <nav className='navbar'>
-                <a href='/'>
-                <img src={process.env.PUBLIC_URL + "/logo.png"} width='60' alt='logo' id='logo' />
-                </a>
+  render() {
+    return (
+      <header>
+        <nav className="navbar">
+          <NavLink to="/">
+            <img
+              src={process.env.PUBLIC_URL + "/logo.png"}
+              width="60"
+              alt="logo"
+              id="logo"
+            />
+          </NavLink>
+          <NavLink to="/" className="nav-logo-link">
+            <div className="navbar-logo-name">
+              <span>Planet's Little Helper</span>
+            </div>
+          </NavLink>
+          <ul
+            className={this.state.clicked ? "nav-menu active" : "nav-menu"}
+            onClick={this.handleClick}
+          >
+            {links.map((link) => (
+              <li key={link.page}>
+                <NavLink to={link.path} className="nav-links">
+                  {link.page}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
 
-                    <div className='navbar-logo-name'>
-                        <span>Planet's Little Helper</span>
-                    </div>
-
-                    <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'} onClick={this.handleClick}>
-                        {links.map(link => <li key={link.page}><NavLink to={link.path} className='nav-links'>{link.page}</NavLink></li>)}
-                    </ul>
-
-                    <div className='navbar-menu-icon' onClick={this.handleClick}>
-                        <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
-                    </div>
-                </nav>
-
-            </header>
-        )
-    }
+          <div className="navbar-menu-icon" onClick={this.handleClick}>
+            <i
+              className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+            ></i>
+          </div>
+        </nav>
+      </header>
+    );
+  }
 }
 
-export default Header
+function mapStateToProps(state) {
+  return {
+    currentUser: state.currentUser,
+  };
+}
+
+export default connect(mapStateToProps, null)(Header);
