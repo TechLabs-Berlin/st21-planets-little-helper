b8c99df58867336e2462ddee78a40bce0826ea56
giuliaeremite <prsgiulia@gmail.com>
2021-06-07 21:15:36 +0200

    example of get request with hooks and axios

--- client/src/pages/userProfile/index.js
+++ client/src/pages/userProfile/index.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function UserProfile() {
-    return (
-        <div>
-            <h1>User Profile</h1>
-        </div>
-    )
+  const [challenges, setChallenges] = useState();
+
+  useEffect(() => {
+    axios("http://localhost:8000/api/challenges")
+      .then((response) => setChallenges(response.data))
+      .catch((e) => {
+        console.log(e);
+      });
+  }, [challenges]);
+
+  return (
+    <div>
+      <h1>User Profile</h1>
+      {challenges &&
+        challenges.map((c) => (
+          <div key={c._id}>
+            <h1>
+              {c.title} - {c.category}
+            </h1>
+            <p>{c.description}</p>
+          </div>
+        ))}
+    </div>
+  );
 }
 
-export default UserProfile
+export default UserProfile;
