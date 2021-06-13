e722bd69c45b9ad865cc5ab42a46f49e81f54f8c
InaDavidova <ina.davidovaa@gmail.com>
2021-06-13 06:36:41 +0300

    Categories page CSS

--- client/src/pages/categories/index.js
+++ client/src/pages/categories/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
-// import { Link } from "react-router-dom"
+import { Link } from "react-router-dom"
 import './categories.css'
 
 
 const categories = [
-    { category: "Food", description: "This respond to basic needs and bring a better quality of life, while minimising the use of natural resources, toxic materials and emissions of waste and pollutants.", path: "/challenges/food", img: "/images/food.png" },
-    { category: "Travel", description: "Green transportation revolves around efficient and effective use of resources, modification of the transport structure and making healthier travel choices.", path: "/challenges/transportation-and-travel", img: "/images/travel.png" },
-    { category: "Energy", description: "Recycling prevents the waste of useful materials and reduces the consumption of fresh raw materials, thereby reducing: energy usage, air and water pollution.", path: "/challenges/energy-consumption", img: "/images/energy.png" },
-    { category: "Recycling", description: "Green energy reduces the negative effects of fossil fuels. Derived from natural resources, it is also often renewable and clean, thus emitting no or few greenhouse gases.", path: "/challenges/recycling", img: "/images/recycle.png" },
+    { category: "Food", description: "This respond to basic needs and bring a better quality of life, while minimising the use of natural resources, toxic materials and emissions of waste and pollutants.", path: "/challenges/food", img: "/images/food1.png" },
+    { category: "Travel", description: "Green transportation revolves around efficient and effective use of resources, modification of the transport structure and making healthier travel choices.", path: "/challenges/transportation-and-travel", img: "/images/travel1.png" },
+    { category: "Energy", description: "Recycling prevents the waste of useful materials and reduces the consumption of fresh raw materials, thereby reducing: energy usage, air and water pollution.", path: "/challenges/energy-consumption", img: "/images/energy1.png" },
+    { category: "Recycling", description: "Green energy reduces the negative effects of fossil fuels. Derived from natural resources, it is also often renewable and clean, thus emitting no or few greenhouse gases.", path: "/challenges/recycling", img: "/images/recycle1.png" },
 ]
 
 function AllCategories() {
@@ -19,21 +19,28 @@ function AllCategories() {
             <div className='container-div'>
 
                 {categories.map(cat => (
-                    <div className='category-box' key={cat.category}>
+                    <a href={cat.path}>
+                        <div className='category-box' key={cat.category}>
 
-                        <h4>{cat.category}</h4>
-                        <div className='imgDiv'>
-                            <a href={cat.path}>
-                                <img src={process.env.PUBLIC_URL + cat.img} alt={cat.category} />
-                            </a>
+                            <img src={process.env.PUBLIC_URL + cat.img} alt={cat.category} />
+                            <h4>{cat.category}</h4>
+
+                            <p>{cat.description}</p>
+
+
                         </div>
-                        <p>{cat.description}</p>
-
-
-                    </div>
+                    </a>
                 ))}
 
 
+            </div>
+
+
+            <h2 className = "categories_h2">Start with small positive changes for our planet</h2>
+            <div className="btn_div">
+                <Link to="/form">
+                    <button className="button-register" id="register_cattegories_page">Register</button>
+                </Link>
             </div>
         </div>
     )
