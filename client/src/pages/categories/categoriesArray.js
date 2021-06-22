eadba95297aa63de52236b099f097bef9fe7b72f
InaDavidova <ina.davidovaa@gmail.com>
2021-06-22 19:29:21 +0300

    Fixed typo in the Food category description

--- client/src/pages/categories/categoriesArray.js
+++ client/src/pages/categories/categoriesArray.js
@@ -1,5 +1,5 @@
 const categories = [
-    { category: "Food", description: "Feed your way into a better quality of life, whilst miniimsing the use of natural resources, toxic materials and emissions of waste and pollutants", path: "/challenges/food", img: "/images/food1.png" },
+    { category: "Food", description: "Feed your way into a better quality of life, whilst minimising the use of natural resources, toxic materials and emissions of waste and pollutants", path: "/challenges/food", img: "/images/food1.png" },
     { category: "Travel", description: "Green transport is all about efficient and effective use of resources, changing methods of transport, and making healthier travel choices", path: "/challenges/travel", img: "/images/travel1.png" },
     { category: "Energy", description: "Green energy reduces the negative affects of fossil fuels. Derived from natural resources, it is also often renewable and clean - therefore emitting little to no greenhouse gases", path: "/challenges/energy", img: "/images/energy1.png" },
     { category: "Recycling", description: "Recycling prevents waste and reduces the production of fresh materials, thereby reducing energy, air and water pollution", path: "/challenges/recycling", img: "/images/recycle1.png" },
