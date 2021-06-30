9bde78da34e20f0c604362cf4b648146c2e583b1
InaDavidova <ina.davidovaa@gmail.com>
2021-06-30 05:55:33 +0300

    Footbar; Link to About page

---
+++ client/src/pages/about/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import styles from "./footer.module.css";
+
+
+function About() {
+    return (
+        <div>
+            <h2>Planet’s Little Helper About Page</h2>
+        </div>
+    )
+}
+
+export default About
