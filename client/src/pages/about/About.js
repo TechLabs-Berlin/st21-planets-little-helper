c10afffa08674ae1da1969d0ca1363382d853615
InaDavidova <ina.davidovaa@gmail.com>
2021-07-08 02:38:50 +0300

    Text on the "About" page added

--- client/src/pages/about/About.js
+++ client/src/pages/about/About.js
@@ -7,8 +7,15 @@ function About() {
     return (
         <div className={styles.contentBox}>
             <div className={styles.aboutDiv}>
+
                 <h2>About PLH</h2>
-                <p>Nice to have you on our journey to have sustainable and positive impact on our environment. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. </p>
+
+                <p>Planet’s Little Helper is a project designed for users beginning their journey in leading a more environmentally-friendly life.
+                    It’s ideal for those who want to do more, but don’t know where to start and feel a little overwhelmed. </p>
+                <p>The app offers personalised and realistic challenges according to the user’s lifestyle and preferences. Encompassing categories
+                    of food, travel, recycling and energy, the user can select a challenge to carry out each week. Users are able to view their
+                    challenges, and mark them as complete. </p>
+                <p>Planet’s Little Helper was built by an interdisciplinary team of six people from the UX, Web Development and Data Science tracks.</p>
 
                 <div className={styles.btnDiv}>
                     <Link to="/challenges">
