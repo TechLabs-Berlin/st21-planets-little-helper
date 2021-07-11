a50184933139a3054d82fb92fc362d2556f3d022
giuliaeremite <prsgiulia@gmail.com>
2021-07-11 10:10:54 +0200

    corrected mongo connection link

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const url = `mongodb://${process.env.MDB_PASSWORD}:plh_techLabs2021@challenges-shard-00-00.1a54e.mongodb.net:27017,challenges-shard-00-01.1a54e.mongodb.net:27017,challenges-shard-00-02.1a54e.mongodb.net:27017/challenges?ssl=true&replicaSet=atlas-9zj051-shard-0&authSource=admin&retryWrites=true&w=majority`;
+const url = `mongodb://planetslittlehelper:${process.env.MDB_PASSWORD}@challenges-shard-00-00.1a54e.mongodb.net:27017,challenges-shard-00-01.1a54e.mongodb.net:27017,challenges-shard-00-02.1a54e.mongodb.net:27017/plh?ssl=true&replicaSet=atlas-9zj051-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
 // mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
