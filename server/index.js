e5c750b88ae627e0824afc4db30ec34d2ac821b5
giuliaeremite <prsgiulia@gmail.com>
2021-07-22 18:35:45 +0200

    moved connection to db to index file

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
 const port = 8000;
@@ -6,25 +7,69 @@ const errorHandler = require("./handlers/error");
 
 const authRoutes = require("./routes/auth");
 const challengesRoutes = require("./routes/challenges");
+// const { connect } = require("./models/index");
+
+const url = `mongodb://planetslittlehelper:${process.env.MDB_PASSWORD}@challenges-shard-00-00.1a54e.mongodb.net:27017,challenges-shard-00-01.1a54e.mongodb.net:27017,challenges-shard-00-02.1a54e.mongodb.net:27017/plh?ssl=true&replicaSet=atlas-9zj051-shard-0&authSource=admin&retryWrites=true&w=majority`;
+const uri = process.env.MONGODB_URI;
 
 const app = express();
-app.use(cors());
-app.use("/uploads",express.static("uploads"))
-app.use(express.json());
-app.use(express.urlencoded());
 
-app.use("/api/user", authRoutes);
-app.use("/api/challenges", challengesRoutes);
+mongoose.set('bufferCommands', false);
+// app.use(cors());
+// app.use("/uploads", express.static("uploads"));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  let err = new Error("Not found");
-  err.status = 404;
-  next(err);
-});
+// app.use("/api/user", authRoutes);
+// app.use("/api/challenges", challengesRoutes);
 
-app.use(errorHandler);
+// app.use((req, res, next) => {
+//   let err = new Error("Not found");
+//   err.status = 404;
+//   next(err);
+// });
 
-app.listen(process.env.PORT || port, () => {
-  console.log(process.env.PORT)
-  console.log(`Server started.`);
-});
+// app.use(errorHandler);
+
+// connect();
+
+// app.listen(process.env.PORT || port, () => {
+//   console.log(`Server started.`);
+// });
+
+async function main() {
+  await mongoose
+    .connect(uri || url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify:false,
+    })
+    .then(() => {
+      console.log("db connected");
+    })
+    .catch((error) => console.log("error connecting to DB : ", error));
+
+  app.use(cors());
+  app.use("/uploads", express.static("uploads"));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.use("/api/user", authRoutes);
+  app.use("/api/challenges", challengesRoutes);
+
+  app.use((req, res, next) => {
+    let err = new Error("Not found");
+    err.status = 404;
+    next(err);
+  });
+
+  app.use(errorHandler);
+
+  app.listen(process.env.PORT || port, () => {
+    console.log(`Server started.`);
+  });
+}
+
+main();
+
--- server/models/index.js
+++ server/models/index.js
@@ -1,17 +1,20 @@
-const mongoose = require("mongoose");
-const url = `mongodb://planetslittlehelper:${process.env.MDB_PASSWORD}@challenges-shard-00-00.1a54e.mongodb.net:27017,challenges-shard-00-01.1a54e.mongodb.net:27017,challenges-shard-00-02.1a54e.mongodb.net:27017/plh?ssl=true&replicaSet=atlas-9zj051-shard-0&authSource=admin&retryWrites=true&w=majority`;
+// require("dotenv").config();
+// const mongoose = require("mongoose");
+// const url = `mongodb://planetslittlehelper:${process.env.MDB_PASSWORD}@challenges-shard-00-00.1a54e.mongodb.net:27017,challenges-shard-00-01.1a54e.mongodb.net:27017,challenges-shard-00-02.1a54e.mongodb.net:27017/plh?ssl=true&replicaSet=atlas-9zj051-shard-0&authSource=admin&retryWrites=true&w=majority`;
+// const uri = process.env.MONGODB_URI;
 
-mongoose.connect(
-  url,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  function (err, res) {
-    try {
-      console.log("Connected to Database");
-    } catch (err) {
-      throw err;
-    }
-  }
-);
+// exports.connect = async () => {
+//   await mongoose
+//     .connect(uri || url, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       useCreateIndex: true,
+//     })
+//     .then(() => {
+//       console.log("db connected");
+//     })
+//     .catch((error) => console.log("error connecting to DB : ", error));
+// };
 
 module.exports.User = require("./user");
 module.exports.ChallengeModel = require("./challenge");
